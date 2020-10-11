e35af70b155e632d5b299acd94398361c5e93d6f
armelyara <theday.ay@gmail.com>
2020-10-11 14:31:54 +0000

    first commit

---
+++ plugins/gmaps/geolocation.js
@@ -0,0 +1,30 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: {
+            lat: 5.4286469,
+            lng: -3.9305231
+        }
+    });
+    const geocoder = new google.maps.Geocoder();
+    document.getElementById("submit").addEventListener("click", () => {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("address").value;
+    geocoder.geocode({
+        address: address
+    }, (results, status) => {
+        if (status === "OK") {
+            resultsMap.setCenter(results[0].geometry.location);
+            new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
